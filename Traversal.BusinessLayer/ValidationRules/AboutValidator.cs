e2c5e9afc94bbc7f13526efd5fa1cffad25f0b59
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2024-02-08 22:22:31 +0300

    Fluent Validation

---
+++ Traversal.BusinessLayer/ValidationRules/AboutValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Traversal.EntityLayer.Concrete;
+
+namespace Traversal.BusinessLayer.ValidationRules
+{
+    public class AboutValidator : AbstractValidator<About>
+    {
+        public AboutValidator()
+        {
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Açıklama Kısmını Boş Geçemezsiniz...!");
+            RuleFor(x => x.Image1).NotEmpty().WithMessage("Lütfen Görsel Seçiniz...!");
+            RuleFor(x => x.Description).MinimumLength(50).WithMessage("Lütfen en az 50 Karakterlik Açıklama Bilgisi Giriniz...!");
+            RuleFor(x => x.Description).MaximumLength(1500).WithMessage("Lütfen Açıklama Bilgisini Kısaltınız...!");
+        }
+    }
+}
